c4c9d6fba900737929a1c1c901eb392147809573
JC Ho <leiadog11@gmail.com>
2020-02-25 09:58:42 -0700

    Create CheckPos.cs

---
+++ CheckPos.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPos : MonoBehaviour
+{
+    int checkPos = 0;
+
+    private void Start()
+    {
+        Debug.Log(checkPos);
+    }
+
+}
+
+    
+

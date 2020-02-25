9439a6aaaa39d42fa1734f1356e217e8fe584e99
JC Ho <leiadog11@gmail.com>
2020-02-25 10:32:38 -0700

    Create ButtonClick.cs

---
+++ ButtonClick.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour
+{
+    public static int buttonValue = 0;
+
+    public void buttonClick()
+    {
+        Debug.Log(buttonValue);
+        
+    }
+
+    public void Update()
+    {
+        buttonValue = buttonValue + 1;
+    }
+}
